25371117d911360e7c1d42854f11987ceda2462e
Storm Muller <stormmuller2@gmail.com>
2018-12-05 16:27:10 +0200

    Finished day 1 part 1

--- day1/Frequency.cpp
+++ day1/Frequency.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <stdlib.h> 
+#include <fstream>
+#include <sstream>
+#include <string>
 using namespace std;
 
 int main() {
-   cout << "Hello World";
+
+   ifstream file("input.txt");
+   string line;
+   int total = 0;
+
+   while(std::getline(file, line)) 
+   {
+      int num = 0;
+      num = stoi(line);
+
+      total += num;
+
+      cout << total << "\n";
+   }
 
    cin.get();
-
    return 0;
 }
